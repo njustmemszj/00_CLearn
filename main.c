b261839679cc66da233556c9c5bc55f301d907b2
JasonZhou <kerui866@163.com>
2019-08-28 16:30:23 +0800

    “”

--- main.c
+++ main.c
@@ -1,43 +1,34 @@
-/* friends.c -- uses pointer to a structure */
+/* friends.global_var -- uses pointer to a structure */
 #include <stdio.h>
-#define LEN 20
 
-struct names {
-    char first[LEN];
-    char last[LEN];
-};
+int global_init_var = 3;
+int global_var;
 
-struct guy {
-    struct names handle;
-    char favfood[LEN];
-    char job[LEN];
-    float income;
-};
+static int statci_global_init_var = 4;
+static int statci_global_var;
 
 int main(void)
 {
-    struct guy fellow[2] = {
-        {{ "Ewen", "Villard"},
-            "grilled salmon",
-            "personality coach",
-            68112.00
-        },
-        {{"Rodney", "Swillbelly"},
-            "tripe",
-            "tabloid editor",
-            432400.00
-        }
-    };
-    struct guy * him;    /* here is a pointer to a structure */
-    
-    printf("address #1: %p #2: %p\n", &fellow[0], &fellow[1]);
-    him = &fellow[0];    /* tell the pointer where to point  */
-    printf("pointer #1: %p #2: %p\n", him, him + 1);
-    printf("him->income is $%.2f: (*him).income is $%.2f\n",
-           him->income, (*him).income);
-    him++;               /* point to the next structure      */
-    printf("him->favfood is %s:  him->handle.last is %s\n",
-           him->favfood, him->handle.last);
-    
+    int local_init_var = 3;
+    static int static_local_init_var = 3;
+    const int local_const = 3;
+    char *p_char = "Hello world";
+    char CharArry[] = "Hello world";
+
+    printf("global_init_var         £º%p \n",&global_init_var);
+    printf("statci_global_init_var  £º%p \n",&statci_global_init_var);
+    printf("\n");
+
+    printf("global_var              £º%p \n",&global_var);
+    printf("statci_global_var       £º%p \n",&statci_global_var);
+    printf("p_char £º%p \n",p_char);
+    printf("\n");
+
+    printf("local_init_var          £º%p \n",&local_init_var);
+    printf("static_local_init_var   £º%p \n",&static_local_init_var);
+    printf("local_const             £º%p \n",&local_const);
+    printf("CharArry                £º%p \n",&CharArry);
+    printf("\n");
+
     return 0;
 }
