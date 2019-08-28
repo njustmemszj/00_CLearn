4bce9d303fb4961f19f22ab4fce5df2561c077ec
JasonZhou <kerui866@163.com>
2019-08-28 16:52:25 +0800

    test *p++

--- main.c
+++ main.c
@@ -1,34 +1,22 @@
 /* friends.global_var -- uses pointer to a structure */
 #include <stdio.h>
 
-int global_init_var = 3;
-int global_var;
-
-static int statci_global_init_var = 4;
-static int statci_global_var;
 
 int main(void)
 {
-    int local_init_var = 3;
-    static int static_local_init_var = 3;
-    const int local_const = 3;
-    char *p_char = "Hello world";
-    char CharArry[] = "Hello world";
+    
+    int arr[] = {1,2,3,4,5};
+    int *p = arr;
+    printf("arr address is  : %p \n",p);
+    printf("-----\n");
 
-    printf("global_init_var         £º%p \n",&global_init_var);
-    printf("statci_global_init_var  £º%p \n",&statci_global_init_var);
-    printf("\n");
-
-    printf("global_var              £º%p \n",&global_var);
-    printf("statci_global_var       £º%p \n",&statci_global_var);
-    printf("p_char £º%p \n",p_char);
-    printf("\n");
-
-    printf("local_init_var          £º%p \n",&local_init_var);
-    printf("static_local_init_var   £º%p \n",&static_local_init_var);
-    printf("local_const             £º%p \n",&local_const);
-    printf("CharArry                £º%p \n",&CharArry);
-    printf("\n");
-
+for (int i =0;i <5;i++)
+{
+    printf("p address is    : %p \n",p);
+    printf("the value is    : %d \n",*p++); // *p first and ++
+    printf("p++ address is  : %p \n",p);
+    printf("-----\n");
+}
+    
     return 0;
 }
